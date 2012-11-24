2b7b6d67e3f832922cf3a74fcb3fe8812424e0d5
Jamie Ridler <jamieridler@gmail.com>
2012-11-24 17:03:40 +0000

    Added more registers and a couple new instructions.

--- src/MAlice/CodeGeneration/ASM.hs
+++ src/MAlice/CodeGeneration/ASM.hs
@@ -2,8 +2,17 @@ module MAlice.CodeGeneration.ASM where
 
 import Data.Word
 
-data Register = RAX | RBX | RCX | RDX
-data Instruction = Mov
+data Register 
+  = RAX | RBX | RCX | RDX | R8 | R9 | R10 | R11 | R12 | R13 | R14 
+  deriving (Show, Eq)
+
+data Instruction 
+  = Mov Register Register
+  | Push Register
+  | Pop Register
+  | Add Register Register
+  | Sub Register Register
+  deriving (Show, Eq)
 
 type ImmVal = Word64
 type Adress = Word64
