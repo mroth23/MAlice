52a66be1f33ca902b872f01b34072e009d6b4e87
Jamie Ridler <jamieridler@gmail.com>
2012-11-24 23:33:42 +0000

    Updated definitions and inclusion of register list

--- src/MAlice/CodeGeneration/AssemblyCodeGenerator.hs
+++ src/MAlice/CodeGeneration/AssemblyCodeGenerator.hs
@@ -4,9 +4,11 @@ import MAlice.CodeGeneration.ASM
 import MAlice.Language.AST
 import MAlice.Language.Types
 
+-- List of available registers for use.
 allRegs :: [Register]
 allRegs =  [RAX, RBX, RCX, RDX, R8, R9, R10, R11, R12, R13, R14]
 
+-- Save free registers onto the stack.
 saveRegs :: [Register] -> [IO ()]
 saveRegs regsNotInUse = saveRegs' allRegs regsNotInUse
 saveRegs' :: [Register] -> [Register] -> [IO ()]
@@ -15,6 +17,7 @@ saveRegs' (x:xs) regsNotInUse
   | elem x regsNotInUse = saveRegs' xs regsNotInUse
   | otherwise           = [putStrLn (show (Push x))] ++ saveRegs' xs regsNotInUse
 
+-- Restore the registers from the stack.
 restRegs :: [Register] -> [IO ()]
 restRegs regsNotInUse = reverse (restRegs' allRegs regsNotInUse)
 restRegs' :: [Register] -> [Register] -> [IO ()]
@@ -23,130 +26,140 @@ restRegs' (x:xs) regsNotInUse
   | elem x regsNotInUse = restRegs' xs regsNotInUse
   | otherwise           = [putStrLn (show (Pop x))] ++ restRegs' xs regsNotInUse
 
+-- Takes a program and gives and output.
 generateCode :: Program -> IO ()
 generateCode (Program (DeclList [])) 
   = return ()
 generateCode (Program (DeclList (decl:decls)))
   = outputSequence ((generateDecl decl allRegs) ++ [generateCode (Program (DeclList decls))])
 
+-- Turns a decleration and a list of available registers and gives a list of output.
 generateDecl :: Decl -> [Register] -> [IO ()]
-generateDecl (VarDecl t ident) regsNotInUse
-  | t == Sentence = [] -- This is a string and should be defined in _global text seperately.
-  | t == Letter   = undefined
-  | otherwise     = undefined
-generateDecl (VAssignDecl t ident expr) regsNotInUse
-  = undefined
+generateDecl (VarDecl _ _) _
+  = [] -- We don't need to write code to declare a variable.
+generateDecl (VAssignDecl _ ident expr) rest
+  = generateExpr expr rest -- Leave the result of the expression in the first free register, track location.
 generateDecl (VArrayDecl t ident expr) regsNotInUse
-  = undefined
+  = undefined -- Make space on the stack and track location.
 generateDecl (FuncDecl ident (FPList formalParams) t body) regsNotInUse
-  = undefined
+  = [putStrLn (show (Colon ident))] ++ (generateFormalParams formalParams regsNotInUse) ++ (generateBody body regsNotInUse) -- Write the function header, write the formalParams and then write the body.
 generateDecl (ProcDecl ident (FPList formalParams) body) regsNotInUse
-  = undefined
+  = undefined -- Write the procedure header, write the formalParams and then write the body.
 
-generateFormalParams :: [FormalParam] -> [IO ()]
-generateFormalParams formalParams
-  = concatMap (generateFormalParam) (formalParams)
+-- Generates code for popping parameters from the stack into free registers.
+generateFormalParams :: [FormalParam] -> [Register] ->  [IO ()]
+generateFormalParams [] _ 
+  = []
+generateFormalParams (f:frest)  (r:rest)
+  = generateFormalParam f (r:rest) ++ generateFormalParams frest rest
 
-generateFormalParam :: FormalParam -> [IO ()]
-generateFormalParam formalParam
-  = undefined
+-- Takes a formalParametres and produces the popping code, track location.
+generateFormalParam :: FormalParam -> [Register] -> [IO ()]
+generateFormalParam formalParam (r:rest)
+  = [putStrLn (show (Pop r))]
 
-generateBody :: Body -> [IO ()]
-generateBody (DeclBody decls comStmt)
+-- Produces the instructions for the body code.
+generateBody :: Body -> [Register] -> [IO ()]
+generateBody (DeclBody decls comStmt) regs
   = undefined
-generateBody (StmtBody comStmt)
+generateBody (StmtBody comStmt) regs
   = undefined
-generateBody EmptyBody
+generateBody EmptyBody regs
   = [return ()]
 
-generateComStmt :: CompoundStmt -> [IO ()]
-generateComStmt (CSList stmts)
-  = concatMap (generateStmt) (stmts)
+generateComStmt :: CompoundStmt -> [Register] -> [IO ()]
+generateComStmt (CSList []) regs
+  = [return ()]
+generateComStmt (CSList (x:xs)) regs
+  = generateStmt x regs ++ generateComStmt (CSList xs) regs
 
-generateStmt :: Stmt -> [IO ()]
-generateStmt (SBody body)
+generateStmt :: Stmt -> [Register] -> [IO ()]
+generateStmt (SBody body) regs
   = undefined
-generateStmt (SNull)
+generateStmt (SNull) regs
   = undefined
-generateStmt (SAssign expr1 expr2)
+generateStmt (SAssign expr1 expr2) regs
   = undefined
-generateStmt (SInc expr)
+generateStmt (SInc expr) regs
   = undefined
-generateStmt (SDec expr)
+generateStmt (SDec expr) regs
   = undefined
-generateStmt (SReturn expr)
+generateStmt (SReturn expr) regs
   = undefined
-generateStmt (SPrint expr)
+generateStmt (SPrint expr) regs
   = undefined
-generateStmt (SInput expr)
+generateStmt (SInput expr) regs
   = undefined
-generateStmt (SCall ident actualParams)
+generateStmt (SCall ident actualParams) regs
   = undefined
-generateStmt (SLoop expr comStmt)
+generateStmt (SLoop expr comStmt) regs
   = undefined
-generateStmt (SIf ((expr, comStmt):rest))
-  = undefined
-
-generateExpr :: Expr -> [IO ()]
-generateExpr (EPlus expr1 expr2)
-  = undefined
-generateExpr (EMinus expr1 expr2)
-  = undefined
-generateExpr (EMult expr1 expr2)
-  = undefined
-generateExpr (EDiv expr1 expr2)
-  = undefined
-generateExpr (EMod expr1 expr2)
-  = undefined
-generateExpr (EBAnd expr1 expr2)
-  = undefined
-generateExpr (EBOr expr1 expr2)
-  = undefined
-generateExpr (EBXor expr1 expr2)
-  = undefined
-generateExpr (ELOr expr1 expr2)
-  = undefined
-generateExpr (ELAnd expr1 expr2)
-  = undefined
-generateExpr (EGT expr1 expr2)
-  = undefined
-generateExpr (EGTE expr1 expr2)
-  = undefined
-generateExpr (EEq expr1 expr2)
-  = undefined
-generateExpr (ELTE expr1 expr2)
-  = undefined
-generateExpr (ELT expr1 expr2)
-  = undefined
-generateExpr (ENEq expr1 expr2)
-  = undefined
-generateExpr (ENot expr)
-  = undefined
-generateExpr (EInv expr)
-  = undefined
-generateExpr (EId exp)
-  = undefined
-generateExpr (EString string)
-  = undefined
-generateExpr (EInt literal)
-  = undefined
-generateExpr (EChar char)
-  = undefined
-generateExpr (EArrRef ident expr)
-  = undefined
-generateExpr (EBkt expr)
-  = undefined
-generateExpr (ECall ident actualParams)
-  = undefined
-generateExpr (ENegate expr)
-  = undefined
-generateExpr (EPositive expr)
+generateStmt (SIf ((expr, comStmt):rest)) regs
   = undefined
 
+generateExpr :: Expr -> [Register] -> [IO ()]
+generateExpr (EPlus expr1 expr2) regs
+  = undefined
+generateExpr (EMinus expr1 expr2) regs
+  = undefined
+generateExpr (EMult expr1 expr2) regs
+  = undefined
+generateExpr (EDiv expr1 expr2) regs
+  = undefined
+generateExpr (EMod expr1 expr2) regs
+  = undefined
+generateExpr (EBAnd expr1 expr2) regs
+  = undefined
+generateExpr (EBOr expr1 expr2) regs
+  = undefined
+generateExpr (EBXor expr1 expr2) regs
+  = undefined
+generateExpr (ELOr expr1 expr2) regs
+  = undefined
+generateExpr (ELAnd expr1 expr2) regs
+  = undefined
+generateExpr (EGT expr1 expr2) regs
+  = undefined
+generateExpr (EGTE expr1 expr2) regs
+  = undefined
+generateExpr (EEq expr1 expr2) regs
+  = undefined
+generateExpr (ELTE expr1 expr2) regs
+  = undefined
+generateExpr (ELT expr1 expr2) regs
+  = undefined
+generateExpr (ENEq expr1 expr2) regs
+  = undefined
+generateExpr (ENot expr) regs
+  = undefined
+generateExpr (EInv expr) regs
+  = undefined
+generateExpr (EId exp) regs
+  = undefined
+generateExpr (EString string) regs
+  = undefined
+generateExpr (EInt literal) regs
+  = undefined
+generateExpr (EChar char) regs
+  = undefined
+generateExpr (EArrRef ident expr) regs
+  = undefined
+generateExpr (EBkt expr) regs
+  = undefined
+generateExpr (ECall ident actualParams) regs
+  = undefined
+generateExpr (ENegate expr) regs
+  = undefined
+generateExpr (EPositive expr) regs
+  = undefined
 
-generateActualParams :: [Expr] -> [IO ()]
-generateActualParams exprs
-  = concatMap (generateExpr) exprs
+
+-- Generate code for producing parameters and push them right to left onto stack.
+generateActualParams :: [Expr] -> [Register] -> [IO ()]
+generateActualParams [] _ 
+  = []
+generateActualParams (x:xs) (r:rest)
+  = generateActualParams xs (r:rest) ++ generateExpr x (r:rest) ++ [putStrLn (show (Push r))]
 
 outputSequence :: [IO ()] -> IO ()
 outputSequence [] = return ()
