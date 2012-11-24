9dabbb9773e4228d479b788fc76b8a81b655a0f1
Jamie Ridler <jamieridler@gmail.com>
2012-11-24 21:45:15 +0000

    Added variable store data types and functions.

--- src/MAlice/CodeGeneration/ASM.hs
+++ src/MAlice/CodeGeneration/ASM.hs
@@ -1,5 +1,7 @@
 module MAlice.CodeGeneration.ASM where
 
+import MAlice.Language.Types
+import MAlice.Language.AST
 import Data.Word
 
 data Register 
@@ -12,9 +14,42 @@ data Instruction
   | Pop Register
   | Add Register Register
   | Sub Register Register
+  | Colon String
   deriving (Show, Eq)
 
 type ImmVal = Word64
 type Adress = Word64
 
--- etc
+type VariableStore = [VariableLoc]
+
+data VariableLoc
+  = Regis String Register | Addre String Int
+
+-- Returns a variable location given a string key and a variable store.
+getVariableLoc :: String -> VariableStore -> VariableLoc
+getVariableLoc key []
+  = error "Key not found in variable store."
+getVariableLoc key ((Regis str reg):rest)
+  | key == str = Regis str reg
+getVariableLoc key ((Addre str int):rest)
+  | key == str = Addre str int
+getVariableLoc key (_:rest)
+  = getVariableLoc key rest
+
+
+-- Inserts a variable location into the variable store.
+insertVariableLoc :: VariableLoc -> VariableStore -> VariableStore
+insertVariableLoc varLoc varStore 
+  = varStore ++ varLoc
+
+
+-- Removes variable location store, I hope there is only one in there...
+removeVariableLoc :: String -> VariableStore -> VariableStore
+removeVariableLoc key []
+  = []
+removeVariableLoc key ((Regis str reg):xs)
+  | key == str = xs
+removeVariableLoc key ((Addre str int):xs)
+  | key == str = xs
+removeVariableLoc key (_:xs)
+  = removeVariableLoc key xs
