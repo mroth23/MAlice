583444d18a5387e69869242fa953ce61871f58a2
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-11-25 17:43:30 +0000

    Finish type checker

--- src/MAlice/SemanticAnalysis/TypeChecker.hs
+++ src/MAlice/SemanticAnalysis/TypeChecker.hs
@@ -15,7 +15,7 @@ succeed :: Type -> TestResult
 succeed = Right
 
 fail :: (Show a) => String -> a -> TestResult
-fail s t = Left ("Expected " ++ s ++ "\nActually got " ++ show t)
+fail s t = Left ("Expected " ++ s ++ ", got " ++ show t)
 
 testUNumOp :: Type -> TestResult
 testUNumOp t =
@@ -52,6 +52,8 @@ testEqOp t1 t2 =
   else fail "two equal, comparable types" (t1, t2)
 
 testAssignOp :: Type -> Type -> TestResult
+testAssignOp (RefType _) _ =
+  Left "can't assign a to a reference type (const)"
 testAssignOp t1 t2 =
   if (t1 == t2)
   then succeed t1
@@ -80,50 +82,95 @@ inferType ex =
     EInv e1      -> inferUnary testUNumOp e1
     ENegate e1   -> inferUnary testUNumOp e1
     EPositive e1 -> inferUnary testUNumOp e1
-    EId var      -> getVarType var
+    EId var      -> getIdType var IdVariable
     EString _    -> return Sentence
     EInt _       -> return Number
     EChar _      -> return Letter
     EArrRef v _  -> getArrayType v
     EBkt expr    -> inferType expr
-    ECall f _    -> do
-      fdecl <- getDecl f
-      return . fromJust . returnType $ fdecl
+    ECall f args -> getIdType f IdFunction
 
-getVarType :: String -> MParser Type
-getVarType var = do
-  ts <- getSymbolTables
-  let v = lookupInTables var ts
+getIdType :: String -> IdentifierType -> MParser Type
+getIdType var expected = do
+  v <- findGlobalIdentifier var
   case v of
     Nothing -> (logError . UnknownIdentifierError $ var) >> return Number
+    (Just e) ->
+      if idType e == expected
+      then return . fromJust . returnType $ e
+      else (logError . InvalidIdKindError $ var ++ " is of kind " ++
+           show (idType e) ++ ", expected " ++ show expected) >> return Number
 
 getArrayType :: String -> MParser Type
 getArrayType var = do
-  t <- getVarType var
+  t <- getIdType var IdVariable
   case t of
-    (RefType t') -> return t
+    (RefType t') -> return t'
     _            -> (logError . TypeError $
                     "expected reference (array) type, got " ++ show t)
                     >> return t
 
-getDecl :: String -> MParser SymbolTableEntry
-getDecl = undefined
+checkFuncCall :: String -> ActualParams -> MParser ()
+checkFuncCall = checkCall IdFunction
 
-checkFuncCall :: Expr -> MParser ()
-checkFuncCall expr@(ECall f args) = do
-  f' <- liftM (lookupInTables f) $ getSymbolTables
-  case idType (fromJust f') of
-    IdFunction -> checkArgs args (argumentTypes . fromJust $ f') f
-    k -> logError . InvalidCalleeError $ f ++ " is of kind " ++ show k
+checkProcCall :: String -> ActualParams -> MParser ()
+checkProcCall = checkCall IdProcedure
 
-checkArgs :: ActualParams -> [Type] -> String -> MParser ()
-checkArgs aps ts name = do
+checkCall :: IdentifierType -> String -> ActualParams -> MParser ()
+checkCall idtype f args =
+  withIdKindCheck idtype f $ \t -> checkArgs args t
+
+-- |Checks whether an identifier is of a specified kind. Also checks whether the
+--  identifier exists by means of 'withIdExistenceCheck'.
+withIdKindCheck :: IdentifierType -> String ->
+                   (SymbolTableEntry -> MParser ()) -> MParser ()
+withIdKindCheck idtype f success =
+  withIdExistenceCheck f $ \e ->
+  if idType e == idtype
+  then success e
+  else logError . InvalidIdKindError $ f ++ " is of kind "
+       ++ (show . idType) e ++  ", expected " ++ show idtype
+
+-- |Checks whether the identifier f exists. If it does, success is applied to
+-- the corresponding 'SymbolTableEntry'. Otherwise, an error is logged.
+withIdExistenceCheck :: String -> (SymbolTableEntry -> MParser ()) -> MParser ()
+withIdExistenceCheck f success = do
+  f' <- findGlobalIdentifier f
+  case f' of
+    Nothing -> logError . UnknownIdentifierError $ f
+    Just e -> success e
+
+checkAssignment :: Expr -> Expr -> MParser ()
+checkAssignment e1 e2 = do
+  t1 <- inferType e1
+  t2 <- inferType e2
+  case testAssignOp t1 t2 of
+    Right _  -> return ()
+    Left err -> logError . TypeError $ err
+
+checkArgs :: ActualParams -> SymbolTableEntry -> MParser ()
+checkArgs aps f = do
   ps <- inferAParamTypes aps
+  let ts = argumentTypes f
   if (and $ zipWith (==) ts ps)
     then return ()
     else logError . CallTypeError $
-         name ++ " expects " ++ (unwords . map show $ ts) ++
-         ", got" ++ (unwords . map show $ ps)
+         (idString f) ++ " expects " ++ (unwords . map show $ ts) ++
+         ", got " ++ (unwords . map show $ ps)
+
+checkInput :: Expr -> MParser ()
+checkInput (EId var) = do
+  v <- getIdType var IdVariable
+  case v of
+    (RefType _) -> logError . TypeError $ "can't read in to reference type"
+    _ -> return ()
+checkInput (EArrRef arr _) = do
+  atype <- getArrayType arr
+  case atype of
+    (RefType _) -> logError . TypeError $ "can't read in to reference type"
+    _ -> return ()
+checkInput _ =
+  logError . TypeError $ "can only read in to variables or array elements"
 
 inferAParamTypes :: ActualParams -> MParser [Type]
 inferAParamTypes (APList aps) =
@@ -150,10 +197,58 @@ inferBinary test e1 e2 = do
     Right t  -> return t
     Left msg -> (logError . TypeError $ msg) >> return t1
 
-typecheckExpr :: Type -> Expr -> MParser ()
-typecheckExpr expected expr = do
+checkExpr :: Type -> Expr -> MParser ()
+checkExpr expected expr = do
   actual <- inferType expr
   if expected == actual
     then return ()
     else logError . TypeError $
          "expected " ++ show expected ++ ", got " ++ show actual
+
+checkReturnType :: Expr -> MParser ()
+checkReturnType e = do
+  f <- getCurrentScope
+  f' <- findGlobalIdentifier f
+  t <- inferType e
+  case f' of
+    Nothing -> logError . InvalidReturnError $ "outside of function body"
+    Just e  ->
+      case idType e of
+        IdFunction ->
+          if t == (fromJust . returnType $ e)
+          then return ()
+          else logError . InvalidReturnError $ "type mismatch, expected " ++
+               show (returnType e) ++ ", got " ++ show t
+        _           ->
+          logError . InvalidReturnError $ "inside a procedure"
+
+-- |Adds the method identifier to the current symbol table, creates a new
+-- symbol table for the method and puts the method's arguments in it.
+enterMethod :: String -> Maybe Type -> IdentifierType ->
+               FormalParams -> MParser ()
+enterMethod f rtype idtype fps@(FPList fpl) = do
+  argtypes <- inferFParamTypes fps
+  insertSymbol f rtype idtype argtypes
+  newSymbolTable f
+  mapM_ (\(Param t var) -> insertSymbol var (Just t) IdVariable []) fpl
+
+-- |Removes the local symbol table as all local variables go out of scope.
+exitMethod :: MParser ()
+exitMethod = removeSymbolTable
+
+enterBlock :: MParser ()
+enterBlock = newSymbolTable ""
+
+exitBlock :: MParser ()
+exitBlock = removeSymbolTable
+
+checkEntryPoint :: MParser ()
+checkEntryPoint = do
+  mainFunc <- findGlobalIdentifier "hatta"
+  case mainFunc of
+    Nothing -> logError . EntryPointError $ "no procedure 'hatta' found"
+    Just e ->
+      if idType e == IdProcedure
+      then return ()
+      else logError . EntryPointError $ "'hatta' is declared as a " ++
+           show (idType e) ++ ", not a procedure"
