729c7b2985ef9e9b8e8508b99df84adfdcf13dc0
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-11-26 12:02:32 +0000

    Change type checker so it stops checking locally if an expression is invalid

--- src/MAlice/SemanticAnalysis/TypeChecker.hs
+++ src/MAlice/SemanticAnalysis/TypeChecker.hs
@@ -69,8 +69,11 @@ testAssignOp t1 t2 =
   then succeed t1
   else fail "two compatible types on assignment operation" (t1, t2)
 
--- |Infers the type of an expression.
-inferType :: Expr -> MParser Type
+-- |Infers the type of an expression. This returns Maybe Type so the type
+-- checker can stop checking expressions with known type errors in them.
+-- Since procedures are specified as returning Nothing, expressions with
+-- procedure calls automatically fail.
+inferType :: Expr -> MParser (Maybe Type)
 inferType ex =
   case ex of
     EPlus e1 e2  -> inferBinary testBNumOp e1 e2
@@ -94,37 +97,40 @@ inferType ex =
     ENegate e1   -> inferUnary testUNumOp e1
     EPositive e1 -> inferUnary testUNumOp e1
     EId var      -> getIdType var IdVariable
-    EString _    -> return Sentence
-    EInt _       -> return Number
-    EChar _      -> return Letter
+    EString _    -> return (Just Sentence)
+    EInt _       -> return (Just Number)
+    EChar _      -> return (Just Letter)
     EArrRef v _  -> getArrayType v
     EBkt expr    -> inferType expr
     ECall f args -> getIdType f IdFunction
 
 -- |Finds the type of an identifier. If it's not found or isn't of the kind
--- IdVariable, errors are logged and a default type is returned so type checking
--- can continue.
-getIdType :: String -> IdentifierType -> MParser Type
+-- IdVariable, errors are logged and a Nothing is returned so type checking
+-- on this expression can't continue.
+getIdType :: String -> IdentifierType -> MParser (Maybe Type)
 getIdType var expected = do
   v <- findGlobalIdentifier var
   case v of
-    Nothing -> (logError . UnknownIdentifierError $ var) >> return Number
-    (Just e) ->
+    Nothing ->
+      (logError . UnknownIdentifierError $ var) >> return Nothing
+    Just e  ->
       if idType e == expected
-      then return . fromJust . returnType $ e
+      then return . returnType $ e
       else (logError . InvalidIdKindError $ var ++ " is of kind " ++
-           show (idType e) ++ ", expected " ++ show expected) >> return Number
+           show (idType e) ++ ", expected " ++ show expected)
+           >> return Nothing
 
 -- |Finds the item type of an array. If the identifier doesn't refer to an array
 -- , an error is logged.
-getArrayType :: String -> MParser Type
+getArrayType :: String -> MParser (Maybe Type)
 getArrayType var = do
   t <- getIdType var IdVariable
-  case t of
-    (RefType t') -> return t'
-    _            -> (logError . TypeError $
-                    "expected reference (array) type, got " ++ show t)
-                    >> return t
+  maybeCheck (return Nothing) t $ \t' ->
+    case t' of
+      (RefType t'') -> return (Just t'')
+      _             -> (logError . TypeError $
+                        "expected reference (array) type, got " ++ show t)
+                       >> return Nothing
 
 -- |Checks that a function call is to a function identifier and has the right
 -- number and types of arguments.
@@ -163,12 +169,23 @@ withIdExistenceCheck f success = do
 
 -- |Checks an assignment between two expressions for validity.
 checkAssignment :: Expr -> Expr -> MParser ()
-checkAssignment e1 e2 = do
-  t1 <- inferType e1
+checkAssignment (EId var) e2 =
+  withIdKindCheck IdVariable var $
+  \e -> checkAssignment' (returnType e) e2
+checkAssignment (EArrRef var _) e2 = do
+  atype <- getArrayType var
+  checkAssignment' atype e2
+checkAssignment e1 _ =
+  logError . TypeError $
+  "Values can only be assigned to variables and array elements"
+
+checkAssignment' :: Maybe Type -> Expr -> MParser ()
+checkAssignment' t1 e2 = do
   t2 <- inferType e2
-  case testAssignOp t1 t2 of
-    Right _  -> return ()
-    Left err -> logError . TypeError $ err
+  maybeCheck2 (return ()) t1 t2 $ \t1' t2' ->
+    case testAssignOp t1' t2' of
+      Right _  -> return ()
+      Left err -> logError . TypeError $ err
 
 -- |Checks whether the argument list given for a method matches its definition.
 -- This function is only called with 'withIdKindCheck', so it doesn't have to
@@ -176,7 +193,7 @@ checkAssignment e1 e2 = do
 checkArgs :: ActualParams -> SymbolTableEntry -> MParser ()
 checkArgs aps f = do
   ps <- inferAParamTypes aps
-  let ts = argumentTypes f
+  let ts = map Just $ argumentTypes f
   if (and $ zipWith (==) ts ps)
     then return ()
     else logError . CallTypeError $
@@ -187,20 +204,24 @@ checkArgs aps f = do
 checkInput :: Expr -> MParser ()
 checkInput (EId var) = do
   v <- getIdType var IdVariable
-  case v of
-    (RefType _) -> logError . TypeError $ "can't read in to reference type"
-    _ -> return ()
+  maybeCheck (return ()) v $ \v' ->
+    case v' of
+      (RefType _) -> logError . TypeError $ "can't read in to reference type"
+      _           -> return ()
+
 checkInput (EArrRef arr _) = do
   atype <- getArrayType arr
-  case atype of
-    (RefType _) -> logError . TypeError $ "can't read in to reference type"
-    _ -> return ()
+  maybeCheck (return ()) atype $ \atype' ->
+    case atype' of
+      (RefType _) -> logError . TypeError $ "can't read in to reference type"
+      _           -> return ()
+
 checkInput _ =
   logError . TypeError $ "can only read in to variables or array elements"
 
 -- |Returns a list of the types of actual parameters
 -- (i.e. a list of expressions)
-inferAParamTypes :: ActualParams -> MParser [Type]
+inferAParamTypes :: ActualParams -> MParser [Maybe Type]
 inferAParamTypes (APList aps) =
   mapM inferType aps
 
@@ -212,30 +233,42 @@ inferFParamTypes (FPList fps) =
   where
     getTypeFromFP (Param t _) = t
 
+-- |Helper function that flips the arguments of maybe around
+maybeCheck :: b -> Maybe a -> (a -> b) -> b
+maybeCheck = flip . maybe
+
+maybeCheck2 :: b -> Maybe a -> Maybe a -> (a -> a -> b) -> b
+maybeCheck2 fail Nothing _ _ = fail
+maybeCheck2 fail _ Nothing _ = fail
+maybeCheck2 _ a a' f = f (fromJust a) (fromJust a')
+
 -- |Helper function used to type-check unary expressions.
 -- To be used in conjunction with any of the testU##### functions.
-inferUnary :: (Type -> TestResult) -> Expr -> MParser Type
+inferUnary :: (Type -> TestResult) -> Expr -> MParser (Maybe Type)
 inferUnary test e1 = do
   t1 <- inferType e1
-  case test t1 of
-    Right t  -> return t
-    Left msg -> (logError . TypeError $ msg) >> return t1
+  maybeCheck (return Nothing) t1 $ \t1 ->
+    case test t1 of
+      Right t  -> return (Just t)
+      Left msg -> (logError . TypeError $ msg) >> return Nothing
 
 -- |Helper function used to type-check binary expressions.
 -- To be used in conjunction with any of the testB##### functions.
-inferBinary :: (Type -> Type -> TestResult) -> Expr -> Expr -> MParser Type
+inferBinary :: (Type -> Type -> TestResult) ->
+               Expr -> Expr -> MParser (Maybe Type)
 inferBinary test e1 e2 = do
   t1 <- inferType e1
   t2 <- inferType e2
-  case (test t1 t2) of
-    Right t  -> return t
-    Left msg -> (logError . TypeError $ msg) >> return t1
+  maybeCheck2 (return Nothing) t1 t2 $ \t1 t2 ->
+    case (test t1 t2) of
+      Right t  -> return (Just t)
+      Left msg -> (logError . TypeError $ msg) >> return Nothing
 
 -- |Checks if an expression has the expected type, if not an error is logged.
 checkExpr :: Type -> Expr -> MParser ()
 checkExpr expected expr = do
   actual <- inferType expr
-  if expected == actual
+  if (Just expected) == actual
     then return ()
     else logError . TypeError $
          "expected " ++ show expected ++ ", got " ++ show actual
@@ -244,19 +277,19 @@ checkExpr expected expr = do
 -- i.e. whether the expression is inside a function of the same type.
 checkReturnType :: Expr -> MParser ()
 checkReturnType e = do
-  f <- getCurrentScope
+  f  <- getCurrentScope
   f' <- findGlobalIdentifier f
-  t <- inferType e
+  t  <- inferType e
   case f' of
     Nothing -> logError . InvalidReturnError $ "outside of function body"
     Just e  ->
       case idType e of
         IdFunction ->
-          if t == (fromJust . returnType $ e)
+          if t == returnType  e
           then return ()
           else logError . InvalidReturnError $ "type mismatch, expected " ++
                show (returnType e) ++ ", got " ++ show t
-        _           ->
+        _          ->
           logError . InvalidReturnError $ "inside a procedure"
 
 -- |Adds the method identifier to the current symbol table, creates a new
