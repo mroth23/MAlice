a44a76a2567b4307e6a005ca5de99e2006de5c6f
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-12-06 23:32:48 +0000

    Beta version of IR code gen

--- src/MAlice/Console.hs
+++ src/MAlice/Console.hs
@@ -2,13 +2,31 @@ module Main where
 
 import System.Environment
 import System.IO
+import qualified MAlice.IR.Types as IR
+import qualified MAlice.IR.CodeGen as IR
 import qualified MAlice.Parser.Parser as MP
-import MAlice.CodeGen.CodeGen
+import qualified MAlice.Parser.ParserState as MP
+import qualified MAlice.Language.AST as AST
+
 main :: IO ()
 main = do
   args <- getArgs
   code <- readFile $ args !! 0
-  case (MP.mparse code $ args !! 0) of
+  let result = compile code (args !! 0)
+  case result of
     Left err -> putStrLn err
-    Right result -> do
-      putStrLn "Program parsed and type-checked successfully!"
+    Right act -> act
+
+compile :: String -> String -> Either String (IO ())
+compile code name = do
+  (parsedAST, _) <- parseCode code name
+  irCode <- generateIR parsedAST
+  return $ IR.showProgram irCode
+
+parseCode :: String -> String -> Either String (AST.Program, MP.ParserState)
+parseCode code name =
+  MP.mparse code name
+
+generateIR :: AST.Program -> Either String IR.Program
+generateIR p =
+  IR.generateIRCode p
---
+++ src/MAlice/IR/Types.hs
@@ -0,0 +1,128 @@
+module MAlice.IR.Types where
+
+import qualified MAlice.Language.Types as MAlice
+import qualified MAlice.Language.AST as AST
+import MAlice.Language.Types
+
+import Data.Maybe
+import qualified Data.Map as M
+import Control.Monad
+import Control.Monad.Identity
+import Control.Monad.State
+
+newtype Program = Program { code :: Code }
+
+showProgram :: Program -> IO ()
+showProgram irp = mapM_ print $ code irp
+
+type Code = [Instr]
+
+type CodeGen a = StateT CState Identity a
+type SymbolTable  = M.Map String String
+
+data CState = CState { lblCount :: Int
+                     , symTables :: [SymbolTable] }
+
+initState = CState { lblCount = 0
+                   , symTables = [M.empty] }
+
+getSymbolTables :: CodeGen [SymbolTable]
+getSymbolTables =
+  symTables `liftM` get
+
+insertSymbol :: String -> String -> CodeGen ()
+insertSymbol k e = do
+  (t:ts) <- getSymbolTables
+  let newt = M.insert k e t
+  updateState $ \st -> st { symTables = newt : ts }
+
+newSymbolTable :: CodeGen ()
+newSymbolTable = do
+  ts <- getSymbolTables
+  updateState $ \st -> st { symTables = M.empty : ts }
+
+removeSymbolTable :: CodeGen ()
+removeSymbolTable = do
+  (t : ts) <- getSymbolTables
+  updateState $ \st -> st { symTables = ts }
+
+updateState :: (CState -> CState) -> CodeGen ()
+updateState f = do
+  st <- get
+  put $ f st
+
+getDefinition :: String -> CodeGen String
+getDefinition v = (fromJust . lookupInTables v) `liftM` getSymbolTables
+
+lookupInTables :: String -> [SymbolTable] -> Maybe String
+lookupInTables _ [] = Nothing
+lookupInTables s (t : ts) =
+  case M.lookup s t of
+    Nothing -> lookupInTables s ts
+    Just e -> Just e
+
+globalPrefix = "__global_"
+paramPrefix = "__param_"
+localPrefix = "__var_"
+methodPrefix = "__m_"
+labelPrefix = "__"
+
+uniqueNumber = do
+  rval <- lblCount `liftM` get
+  updateState $ \st -> st { lblCount = rval + 1 }
+  return (show rval)
+
+uniqueLabel = do
+  rval <- uniqueNumber
+  return $ AId ("__t" ++ rval)
+
+globalLabel var = do
+  uid <- uniqueNumber
+  return $ globalPrefix ++ var ++ "_" ++ uid
+
+paramLabel var = do
+  uid <- uniqueNumber
+  return $ paramPrefix ++ var ++ "_" ++ uid
+
+localLabel var = do
+  uid <- uniqueNumber
+  return $ localPrefix ++ var ++ "_" ++ uid
+
+methodLabel var = do
+  uid <- uniqueNumber
+  return $ methodPrefix ++ var ++ "_" ++ uid
+
+labelLabel var = do
+  uid <- uniqueNumber
+  return $ labelPrefix ++ var ++ "_" ++ uid
+
+data Instr =
+  IAlloc Label                          | --Allocate a new variable
+  IAllocArr Label Operand               | --Allocate an array
+  IAllocParam Label Int MAlice.Type     | --Allocate a function param
+  IAssignB Label Operand String Operand | --x := y `op` z
+  IAssignU Label String Operand         | --x := op y
+  ICopy Label Label                     | --x := y
+  ICall Label Int                       | --x := y(..) with n params
+  IGoto Label                           | --goto Label
+  ICGoto Label Label                    | --if a goto label
+  INCGoto Label Label                   | --if !a goto label
+  ILabel Label                          | --Label:
+  IMLabel Label (Maybe MAlice.Type)     | --Method label
+  IParam Label                          | --Use label as param in call
+  IReturn Label                         | --Return label
+  IPrint Label                          | --print label
+  IExit                                 |
+  IInput Label
+  deriving (Eq, Show)
+
+type Label = Operand
+
+data Operand =
+  AId String              | --id/label
+  AString String          | --String literal
+  AInt Int                | --x := Int literal
+  AChar Char              | --x := Char literal
+  AArrRef Label Operand   | --a[ix]
+  ACall Label Int           --f() with x params
+  deriving (Eq, Show)
