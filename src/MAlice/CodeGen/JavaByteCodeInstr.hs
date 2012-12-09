cd31557a4df70ce18b34cfd5c358fd6bcd8c75f0
Jamie Ridler <jcr111@doc.ic.ac.uk>
2012-12-09 15:55:41 +0000

    Byte code instructions.

--- src/MAlice/CodeGen/JavaByteCodeInstr.hs
+++ src/MAlice/CodeGen/JavaByteCodeInstr.hs
@@ -5,12 +5,18 @@ type JProgram = [JInstr]
 data JInstr =
   Class Label              | -- Class definition with name.
   SuperClass               | -- Default super class is java/lang/Object.
-  Field Label FType        | -- Creates a new variable with label and type.
-  Getfield Label FType     | -- Gets field value from objectref.
-  Putfield Label           | -- Puts a objectref in the field.
-  Call Label FParams RType | -- Call method with params and return type.
+  Constructor JProgram     | -- Constructor for this programs object.
+  Field Label String       | -- Creates a new variable with label and type.
+  Getfield Label String    | -- Gets field value from objectref.
+  Putfield Label String    | -- Puts a objectref in the field.
+  Call Label String String | -- Call method with params and return type.
+  Func Label String String | -- A function.
+  ALoad Int                | -- Load reference onto stack from local var int..
   ALoad_0                  | -- Load reference onto stack from local var 0.
   ALoad_1                  | -- Load object in local variable 1 onto stack.
+  AStore Int               | -- Store object reference from stack in local var.
+  ILoad_0                  |
+  ILoad_1                  |
   ILoad Int                | -- Pushes value of local var. (Integer)
   IStore Int               | -- Pops value into local var. (Integer)
   Ldc Constant             | -- Push constant.
@@ -37,18 +43,111 @@ data JInstr =
   IXor                     | -- Xor two values on stack.
   IConst_0                 | -- Load int value 0 onto stack.
   IConst_1                 | -- Load int value 1 onto stack.
+  IConst_m1                | -- Load int value -1 onto stack.
   I2c                      | -- Convert an int to a character.
-  Newarray AType           | -- Create a new array with elements as int on stack.
+  New String               | -- Create object of type string and place ref on stack.
+  Newarray String          | -- Create a new array with elements as int on stack.
   IALoad                   | -- Load an int from an array. Push array ref, then int index.
   IAStore                  | -- Load an int into an array. Push array ref, then int index, then int.
   IInc Index Constant      | -- Increment a local var by a given constant.
+  BIPush Int               | -- Push char onto stack
+  Getstatic String String  | -- Get a static field value of a class.
+  --            Ident  Param  Return
+  Invokevirtual String String String  | -- Invokes/calls a method.
+  Invokespecial String String String  | -- Special for init.
+  Endmethod                | -- Signals the end of a method.
   IReturn                  | -- Return an integer from a function.
-  Return                   | -- Return void from a method.
+  Return                     -- Return void from a method.
 
+instance Show JInstr where
+  show (Class label)
+    = ".class public " ++ label ++ "\n"
+  show (SuperClass)
+    = ".super java/lang/Object \n"
+  show (Constructor jprogram)
+    = ".method public <init>()V \n"                 ++
+      ".limit stack 100 \b"                         ++
+      ".limit locals 100 \n"                        ++
+      "aload_0 \n"                                  ++
+      "invokespecial java/lang/Object/<init>()V \n" ++
+      show jprogram                                 ++
+      ".end method \n"
+  show (Field label t)
+    = ".field public " ++ label ++ " " ++ t ++ "\n"
+  show (Getfield label return)
+    = "getfield " ++ label ++ " " ++ return ++ "\n"
+  show (Putfield label return)
+    = "putfield " ++ label ++ " " ++ return ++ "\n"
+  show (Call label params return)
+    = "invokevirtual " ++
+       label ++ "(" ++ params ++ ")" ++
+       return ++ "\n"
+  show (Func label params return)
+    = ".method public " ++ 
+       label ++ "(" ++ params ++ ")" ++ 
+       return ++ "\n"
+  show (ALoad num)         = "aload " ++ show num ++ "\n"
+  show (ALoad_0)           = "aload_0" ++ "\n"
+  show (ALoad_1)           =  "aload_1" ++ "\n"
+  show (AStore num)        = "astore " ++ show num ++ "\n"
+  show (ILoad_0)           = "iload_0" ++ "\n"
+  show (ILoad_1)           = "iload_1" ++ "\n"
+  show (ILoad num)         =  "iload " ++ show num ++ "\n"
+  show (IStore num)        = "istore " ++ show num ++ "\n"
+  show (Ldc const)         = "ldc " ++ show const ++ "\n"
+  show (Dup)               = "dup" ++ "\n"
+  show (Pop)               = "pop" ++ "\n"
+  show (Swap)              = "swap" ++ "\n"
+  show (IAdd)              = "iadd" ++ "\n"
+  show (IMul)              = "imul" ++ "\n"
+  show (ISub)              = "isub" ++ "\n"
+  show (IDiv)              = "idiv" ++ "\n"
+  show (IRem)              = "irem" ++ "\n"
+  show (Goto label)        = "goto " ++ label ++ "\n"
+  show (Ifeq label)        = "ifeq " ++ label ++ "\n"
+  show (Ifge label)        = "ifge " ++ label ++ "\n"
+  show (Ifgt label)        = "ifgt " ++ label ++ "\n"
+  show (Ifle label)        = "ifle " ++ label ++ "\n"
+  show (Iflt label)        = "iflt " ++ label ++ "\n"
+  show (Ifne label)        = "ifne " ++ label ++ "\n"
+  show (IAnd)              = "iand" ++ "\n"
+  show (IOr)               = "ior" ++ "\n"
+  show (IShl)              = "ishl" ++ "\n"
+  show (IShr)              = "ishr" ++ "\n"
+  show (IUshr)             = "iushr" ++ "\n"
+  show (IXor)              = "ixor" ++ "\n"
+  show (IConst_0)          = "iconst_0" ++ "\n"
+  show (IConst_1)          = "iconst_1" ++ "\n"
+  show (IConst_m1)         = "iconst_m1" ++ "\n"
+  show (I2c)               = "i2c" ++ "\n"
+  show (New str)           = "new " ++ str ++ "\n"
+  show (Newarray str)      = "newarray " ++ str ++ "\n"
+  show (IALoad)            = "iaload" ++ "\n"
+  show (IAStore)           = "iastore" ++ "\n"
+  show (IInc index const)  = "iinc " ++ show index ++ " " ++ show const ++ "\n"
+  show (BIPush i)          = "bipush " ++ show i ++ "\n"
+  show (IReturn)           = "iret" ++ "\n" 
+  show (Return)            = "return" ++ "\n"
+  show (Getstatic lib obj)
+    = "getstatic " ++ lib ++ " " ++ obj ++ "\n"
+  show (Invokevirtual label param ret)
+    = "invokevirtual " ++
+       label ++ "(" ++ param ++ ")" ++
+       ret ++ "\n"
+  show (Invokespecial label param ret)
+    = "invokespecial " ++ 
+       label ++ "(" ++ param ++ ")" ++
+       ret ++ "\n"
 
 data Constant =
-  ConsI Int   | 
+  ConsI Integer | 
   ConsS String
+
+instance Show Constant where
+  show (ConsI i)
+    = show i
+  show (ConsS str)
+    = show str
 
 type FParams = [FParam]
 data FParam =
@@ -56,10 +155,11 @@ data FParam =
   FString  |
   FArray
 
-data FType =
-  FInt    |
-  FString |
-  FArray  
+{- data FType =
+  FInt        |
+  FString     |
+  FArray FInt |
+  FArray FType -}
 
 type Label = String
 type AType = Int
