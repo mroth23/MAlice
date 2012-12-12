8245bea11d55db88ff6ed2a63716ac056f7091c1
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-12-12 14:34:50 +0000

    Remove unused files

--- src/MAlice/CodeGen/CodeGenState.hs
+++
@@ -1,8 +0,0 @@
-module MAlice.CodeGen.CodeGenState where
-
-import MAlice.Language.SymbolTable
-import MAlice.Parser.ParserState
-
-newtype CodeGenState = CodeGenState { symbolTable :: [SymbolTable] }
-
-initState st = CodeGenState { symbolTable = [] }
